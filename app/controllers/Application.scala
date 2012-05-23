973a123cc9f1da8db11cfd1969a5d6c22dc2f423
Andreas Nüßlein <nutz@noova.de>
2012-05-23 02:46:30 +0200

    added "secure area"

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,12 +3,20 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+import models._
 
-object Application extends Controller {
+object Application extends Controller with Secured {
 
   def index = Action { implicit request =>
     Ok(views.html.index())
   }
+
+  def secure = IsAuthenticated { username => implicit request =>
+    val user = User.findByUsername(username).get
+    Ok(views.html.signup.summary(user))
+  }
+
+
 
 
 }
