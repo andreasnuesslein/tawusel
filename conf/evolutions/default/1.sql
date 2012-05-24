6ac15f47ad90a96937e190e140635bdd853f0439
Andreas Nüßlein <andreas@nuessle.in>
2012-05-24 12:53:21 +0200

    moore sql

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,47 +1,47 @@
 # --- !Ups
 
 CREATE  TABLE IF NOT EXISTS `user` (
-  `user_id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `email` VARCHAR(45) NOT NULL ,
   `forename` VARCHAR(45) NOT NULL ,
   `lastname` VARCHAR(45) NOT NULL ,
   `cellphone` VARCHAR(20) NOT NULL ,
   `password` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`user_id`) ,
+  PRIMARY KEY (`id`) ,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) )
 ENGINE = InnoDB;
 
 CREATE  TABLE IF NOT EXISTS `town` (
-  `town_id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`town_id`) ,
+  PRIMARY KEY (`id`) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) )
 ENGINE = InnoDB;
 
 CREATE  TABLE IF NOT EXISTS `location` (
-  `location_id` INT NOT NULL AUTO_INCREMENT ,
-  `town_location_id` INT NOT NULL ,
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `town_id` INT NOT NULL ,
   `name` VARCHAR(45) NOT NULL ,
   `address` VARCHAR(80) NULL ,
-  PRIMARY KEY (`location_id`) ,
-  INDEX `town_location_id` (`town_location_id` ASC) ,
+  PRIMARY KEY (`id`) ,
+  INDEX `town_id` (`town_id` ASC) ,
   UNIQUE INDEX `name_UNIQUE` (`name` ASC) ,
-  CONSTRAINT `town_location_id`
-    FOREIGN KEY (`town_location_id` )
-    REFERENCES `town` (`town_id` )
+  CONSTRAINT `town_id`
+    FOREIGN KEY (`town_id` )
+    REFERENCES `town` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 CREATE  TABLE IF NOT EXISTS `tour_state` (
-  `state_id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(20) NULL ,
   `description` MEDIUMTEXT NULL ,
-  PRIMARY KEY (`state_id`) )
+  PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 
 CREATE  TABLE IF NOT EXISTS `tour` (
-  `tour_id` INT NOT NULL AUTO_INCREMENT ,
+  `id` INT NOT NULL AUTO_INCREMENT ,
   `date` DATE NOT NULL ,
   `departure` TIME NOT NULL ,
   `arrival` TIME NOT NULL ,
@@ -51,30 +51,30 @@ CREATE  TABLE IF NOT EXISTS `tour` (
   `meetingpoint` VARCHAR(255) NULL ,
   `authentification` VARCHAR(45) NULL ,
   `tour_state` INT NOT NULL ,
-  `mod_user_id` INT NULL ,
-  PRIMARY KEY (`tour_id`) ,
+  `mod_id` INT NULL ,
+  PRIMARY KEY (`id`) ,
   INDEX `location_tour_dep_location` (`dep_location` ASC) ,
   INDEX `location_tour_arr_location` (`arr_location` ASC) ,
   INDEX `tour_state_tour_tour_state` (`tour_state` ASC) ,
-  INDEX `user_tour_mod_user_id` (`mod_user_id` ASC) ,
+  INDEX `user_tour_mod_id` (`mod_id` ASC) ,
   CONSTRAINT `location_tour_dep_location`
     FOREIGN KEY (`dep_location` )
-    REFERENCES `location` (`location_id` )
+    REFERENCES `location` (`id` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `location_tour_arr_location`
     FOREIGN KEY (`arr_location` )
-    REFERENCES `location` (`location_id` )
+    REFERENCES `location` (`id` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT `tour_state_tour_tour_state`
     FOREIGN KEY (`tour_state` )
-    REFERENCES `tour_state` (`state_id` )
+    REFERENCES `tour_state` (`id` )
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
-  CONSTRAINT `user_tour_mod_user_id`
-    FOREIGN KEY (`mod_user_id` )
-    REFERENCES `User` (`user_id` )
+  CONSTRAINT `user_tour_mod_id`
+    FOREIGN KEY (`mod_id` )
+    REFERENCES `user` (`id` )
     ON DELETE SET NULL
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -87,12 +87,12 @@ CREATE  TABLE IF NOT EXISTS `user_has_tour` (
   INDEX `fk_user_has_tour_user1` (`user_id` ASC) ,
   CONSTRAINT `fk_user_has_tour_user1`
     FOREIGN KEY (`user_id` )
-    REFERENCES `User` (`user_id` )
+    REFERENCES `user` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_has_tour_tour1`
     FOREIGN KEY (`tour_id` )
-    REFERENCES `tour` (`tour_id` )
+    REFERENCES `tour` (`id` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -101,11 +101,11 @@ ENGINE = InnoDB;
 -- Data for table `town`
 -- -----------------------------------------------------
 START TRANSACTION;
-INSERT INTO `town` (`town_id`, `name`) VALUES (1, 'Berlin');
-INSERT INTO `town` (`town_id`, `name`) VALUES (2, 'Wolfsburg');
-INSERT INTO `town` (`town_id`, `name`) VALUES (3, 'Ingolstadt');
-INSERT INTO `town` (`town_id`, `name`) VALUES (4, 'Stattgart');
-INSERT INTO `town` (`town_id`, `name`) VALUES (5, 'Prag');
+INSERT INTO `town` (`id`, `name`) VALUES (1, 'Berlin');
+INSERT INTO `town` (`id`, `name`) VALUES (2, 'Wolfsburg');
+INSERT INTO `town` (`id`, `name`) VALUES (3, 'Ingolstadt');
+INSERT INTO `town` (`id`, `name`) VALUES (4, 'Stattgart');
+INSERT INTO `town` (`id`, `name`) VALUES (5, 'Prag');
 
 COMMIT;
 
@@ -113,31 +113,31 @@ COMMIT;
 -- Data for table `Location`
 -- -----------------------------------------------------
 START TRANSACTION;
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (1, 1, 'Carmeq Berlin', 'Carnotstraße 4, 10587 Berlin');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (2, 2, 'Carmeq Wolfsburg, Autovision', 'Major-Hirst-Straße 11, 38442 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (3, 2, 'Volkswagen FE', 'Nordstraße, 38440 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (4, 2, 'Volkswagen TE, Hopfengarten', 'Hopfengarten 37-47, 38442 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (5, 2, 'Volkswagen TE, Rübenkamp', 'Rübenkamp 2, 38442 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (6, 2, 'Volkswagen AutoUni', 'Berliner Ring 2, 38440 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (7, 2, 'Volkswagen LKW Wache', 'Stellfelder Straße 46, 33442 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (8, 2, 'Volkswagen HMI, Fa. Volke', 'Daimlerstraße 38, 38446 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (9, 2, 'Volkswagen Isenbüttel', 'Poststraße 28, 38440 Wolfsburg');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (10, 2, 'Volkswagen Prüfgelände Ehra', 'Prüfgelände, 38468 Ehra-Lessien');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (11, 3, 'Carmeq Ingolstadt', 'Sachsstraße 14b, 85080 Gaimersheim');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (12, 3, 'Audi TE', 'Tor 9, 85055 Ingolstadt');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (13, 3, 'Audi Forum', 'Ettinger Straße, 85057 Ingolstadt');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (14, 4, 'Carmeq Stuttgart', 'Königstraße 43b, 70173 Stuttgart');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (15, 4, 'Porsche Weissach', 'Porschestraße, 71287 Weissach');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (16, 5, 'e4t', 'Novodvorská 994/138, CZ-142 21 Praha 4');
-INSERT INTO `location` (`location_id`, `town_location_id`, `name`, `address`) VALUES (17, 5, 'Ŝkoda Werk', 'Nušlova 2515/4, CZ-158 00 Praha 13');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (1, 1, 'Carmeq Berlin', 'Carnotstraße 4, 10587 Berlin');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (2, 2, 'Carmeq Wolfsburg, Autovision', 'Major-Hirst-Straße 11, 38442 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (3, 2, 'Volkswagen FE', 'Nordstraße, 38440 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (4, 2, 'Volkswagen TE, Hopfengarten', 'Hopfengarten 37-47, 38442 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (5, 2, 'Volkswagen TE, Rübenkamp', 'Rübenkamp 2, 38442 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (6, 2, 'Volkswagen AutoUni', 'Berliner Ring 2, 38440 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (7, 2, 'Volkswagen LKW Wache', 'Stellfelder Straße 46, 33442 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (8, 2, 'Volkswagen HMI, Fa. Volke', 'Daimlerstraße 38, 38446 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (9, 2, 'Volkswagen Isenbüttel', 'Poststraße 28, 38440 Wolfsburg');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (10, 2, 'Volkswagen Prüfgelände Ehra', 'Prüfgelände, 38468 Ehra-Lessien');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (11, 3, 'Carmeq Ingolstadt', 'Sachsstraße 14b, 85080 Gaimersheim');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (12, 3, 'Audi TE', 'Tor 9, 85055 Ingolstadt');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (13, 3, 'Audi Forum', 'Ettinger Straße, 85057 Ingolstadt');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (14, 4, 'Carmeq Stuttgart', 'Königstraße 43b, 70173 Stuttgart');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (15, 4, 'Porsche Weissach', 'Porschestraße, 71287 Weissach');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (16, 5, 'e4t', 'Novodvorská 994/138, CZ-142 21 Praha 4');
+INSERT INTO `location` (`id`, `town_id`, `name`, `address`) VALUES (17, 5, 'Ŝkoda Werk', 'Nušlova 2515/4, CZ-158 00 Praha 13');
 
 COMMIT;
 
 # --- !Downs
 
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS `town` ;
-DROP TABLE IF EXISTS `location` ;
-DROP TABLE IF EXISTS `tour_state` ;
-DROP TABLE IF EXISTS `tour` ;
 DROP TABLE IF EXISTS `user_has_tour` ;
+DROP TABLE IF EXISTS `tour` ;
+DROP TABLE IF EXISTS `tour_state` ;
+DROP TABLE IF EXISTS `location` ;
+DROP TABLE IF EXISTS `town` ;
+DROP TABLE IF EXISTS `user`;
