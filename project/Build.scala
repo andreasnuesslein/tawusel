a270d87cb6f08e196cffa3ebcbb1827b4d861cde
Andreas Nüßlein <andreas@nuessle.in>
2012-05-29 13:27:13 +0200

    we had an old akka-dependency. f5

--- project/Build.scala
+++ project/Build.scala
@@ -9,15 +9,10 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
       "mysql" % "mysql-connector-java" % "5.1.18",
-      "se.scalablesolutions.akka" % "akka-actor" % "1.1.3",
-      "se.scalablesolutions.akka" % "akka-typed-actor" % "1.1.3",
-      "se.scalablesolutions.akka" % "akka-amqp" % "1.1.3",
-      "se.scalablesolutions.akka" % "akka-testkit" % "1.1.3"
-      // Add your project dependencies here,
+      "com.typesafe.akka" % "akka-actor" % "2.+"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
     )
 
 }
