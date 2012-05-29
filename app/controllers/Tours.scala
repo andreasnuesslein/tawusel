bdf3e611801ce8870e62c46ba7f4a8efd5830794
Andreas Nüßlein <andreas@nuessle.in>
2012-05-29 13:04:06 +0200

    added tour-list-dummy

--- app/controllers/Tours.scala
+++ app/controllers/Tours.scala
@@ -6,10 +6,14 @@ import play.api.mvc._
 import models._
 
 object Tours extends Controller with Secured {
-  
-def newTour =IsAuthenticated({ email => implicit request =>
+
+  def newTour = IsAuthenticated({ email => implicit request =>
     val user = User.findByEmail(email).get
     Ok(views.html.start())
   }, Redirect(routes.Application.index))
-  
+
+  def tours = IsAuthenticated({ email => implicit request =>
+    Ok(views.html.tours())
+  })
+
 }
