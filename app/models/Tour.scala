c80f2ec8eea8052fd8167fdac39ddfcefa1016e0
Andreas Nüßlein <andreas@nuessle.in>
2012-06-21 21:10:14 +0200

    tour template jquery magic

--- app/models/Tour.scala
+++ app/models/Tour.scala
@@ -159,9 +159,9 @@ object Tour {
   }
   
 
-  def findAll(userid:Int = -1): List[(Int, String, String, String, java.util.Date, java.util.Date,List[User])] = {
+  def findAll(userid:Int = -1): List[(Int, String, String, String, java.util.Date, java.util.Date, Int, List[(Int,String,String,String,String)], Int)] = {
     DB.withConnection { implicit connection =>
-      var tours = SQL("""select tour.id,town.name,tour.departure,tour.arrival,l1.name as dep_location,l2.name
+      var tours = SQL("""select tour.id,town.name,tour.departure,tour.arrival,l1.name as dep_location,l2.name,tour.mod_id, tour.tour_state
         from tour
         join location  as l1 on dep_location=l1.id
         join location2 as l2 on arr_location=l2.id
@@ -170,10 +170,11 @@ object Tour {
         """).on(
             'id -> userid
           )
-      .as(int("id") ~ str("town.name") ~ str("location.name") ~ str("location2.name") ~ date("departure") ~ date("arrival") map(flatten) * )
-      var x:List[(Int, String, String, String, java.util.Date, java.util.Date,List[User])] = for(t <- tours;
-        p = SQL("""select * from user join user_has_tour on user_has_tour.user_id=user.id where tour_id={tid}""").on('tid -> t._1).as(User.simple *)
-        ) yield (t._1,t._2,t._3,t._4,t._5,t._6,p)
+      .as(int("id") ~ str("town.name") ~ str("location.name") ~ str("location2.name") ~ date("departure") ~ date("arrival") ~ int("mod_id") ~ int("tour_state") map(flatten) * )
+      var x:List[(Int, String, String, String, java.util.Date, java.util.Date, Int, List[(Int,String,String,String,String)], Int)] = for(t <- tours;
+        p = SQL("""select * from user join user_has_tour on user_has_tour.user_id=user.id where tour_id={tid}""").on('tid -> t._1).as(
+          int("id") ~ str("email") ~ str("firstname") ~ str("lastname") ~ str("cellphone") map(flatten) *)
+        ) yield (t._1,t._2,t._3,t._4,t._5,t._6,t._7,p,t._8)
       return x
     }
   }
@@ -210,7 +211,6 @@ object Tour {
   /*This method returns at most the 8 most planned tours for a specified user.
     The list of favorised tours should be chached for one day(24h/86400s).*/
   def findTemplatesForUser(user_id: Int): List[(String, String, String, java.util.Date, java.util.Date)] = {
-    //val templates: List[Tour] = Cache.getOrElse[List[Tour]]("tours.templates", 86400) {
       DB.withConnection { implicit connection =>
         val favorites: List[(String, String, String, java.util.Date, java.util.Date)] = SQL("""SELECT *
           FROM favorites
@@ -239,9 +239,9 @@ object Tour {
   }
 
   /*Displays the ongoing tours for the given user, specified by id.*/
-  def findAllForUser(user_id: Int): List[(Int, String, String, String, java.util.Date, java.util.Date, Int, List[(Int,String,String,String,String)])] = {
+  def findAllForUser(user_id: Int): List[(Int, String, String, String, java.util.Date, java.util.Date, Int, List[(Int,String,String,String,String)], Int)] = {
     DB.withConnection { implicit connection =>
-      var tours = SQL("""SELECT tour.id,town.name,tour.departure,tour.arrival,l1.name as dep_location,l2.name, tour.mod_id
+      var tours = SQL("""SELECT tour.id,town.name,tour.departure,tour.arrival,l1.name as dep_location,l2.name, tour.mod_id, tour.tour_state
         FROM tour
         JOIN user_has_tour ON tour.id = user_has_tour.tour_id
         join location  as l1 on dep_location=l1.id
@@ -250,12 +250,12 @@ object Tour {
         WHERE user_has_tour.user_id = {user_id} AND tour.departure > NOW()"""
       ).on(
         'user_id -> user_id
-      ).as(int("id") ~ str("town.name") ~ str("location.name") ~ str("location2.name") ~ date("departure") ~ date("arrival") ~ int("mod_id") map(flatten) *)
+      ).as(int("id") ~ str("town.name") ~ str("location.name") ~ str("location2.name") ~ date("departure") ~ date("arrival") ~ int("mod_id") ~ int("tour_state") map(flatten) *)
     var x = for(t <- tours;
       p = SQL("""select id, email, firstname, lastname, cellphone
         from user join user_has_tour on user_has_tour.user_id=user.id
         where tour_id={tid}""").on('tid -> t._1).as(int("id") ~ str("email") ~ str("firstname") ~ str("lastname") ~ str("cellphone") map(flatten) *)
-      ) yield (t._1,t._2,t._3,t._4,t._5,t._6,t._7,p)
+      ) yield (t._1,t._2,t._3,t._4,t._5,t._6,t._7,p,t._8)
     return x
     }
 
