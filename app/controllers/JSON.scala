6291c55ed599aad4757fd48f1b2f47f42f8ce3f7
sebastianschulz <deplayer86@web.de>
2012-06-28 12:21:43 +0200

    moved json querys to JSON.scala

---
+++ app/controllers/JSON.scala
@@ -0,0 +1,50 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models._
+import java.sql.Date
+import com.codahale.jerkson.Json
+
+object JSON extends Controller {
+
+  def listLocationsByTown_Id(town_id:String) = Action {
+    val locations = Location.findByTown_id(town_id.toLong);
+    val json = Json.generate(locations)
+    Ok(json).as("application/json")
+  }
+
+  def listToursByTown_Id(town_id:String) = Action {
+    val tours = Tour.findByTown_id(town_id.toLong)
+    val json ="{\"aaData\": "+ Json.generate(tours) + "}"
+    Ok(json).as("application/json")
+  }
+  
+  def listToursByDepLocation_Id(location_id:String) = Action {
+    val tours = Tour.findByDepLocation_id(location_id.toLong)
+    val json ="{\"aaData\": "+ Json.generate(tours) + "}"
+    Ok(json).as("application/json")
+  }
+ 
+  def listToursByArrLocation_Id(locationDep_id:String,locationArr_id:String) = Action {
+    val tours = Tour.findByArrLocation_id(locationDep_id.toLong,locationArr_id.toLong)
+    val json ="{\"aaData\": "+ Json.generate(tours) + "}"
+    Ok(json).as("application/json")
+  }
+
+  def listAllTours() = Action {
+    val tours = Tour.findAll();
+    val json ="{\"aaData\": "+ Json.generate(tours) + "}"
+    Ok(json).as("application/json")
+  }
+
+  def authentificateByApp(email:String, hashedPassword:String) = Action {
+    val user = User.authenticateWithHashedPassword(email, hashedPassword)
+    val json ="{\"aaData\": "+ Json.generate(user) + "}"
+    Ok(json).as("application/json")
+  }
+  
+  
+}
