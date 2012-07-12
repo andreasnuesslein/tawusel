0054f1087adef7767b1fdeb26c9fbeb675517ed5
Andreas Nüßlein <andreas@nuessle.in>
2012-07-12 23:11:11 +0200

    added welcome page :)
    closes nutztherookie/swp_kp2_2012_gruppe1#101

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,10 +5,29 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import models._
+import views._
 
 object Application extends Controller with Secured {
 
-  def index = Action { implicit request =>
+  def index = IsAuthenticated ({ _ => implicit request =>
      Redirect(routes.Tours.tours)
+   },
+   {
+     Redirect(routes.Application.welcome)
+   })
+
+ val loginForm = Form(
+   tuple(
+     "email" -> email,
+     "password" -> text
+   ) verifying ("Invalid email or password", result => result match {
+     //call the authenticate method of the user model to query the actual verifying
+     case (email, password) => User.authenticate(email, password).isDefined
    }
+ )
+                                    )
+
+  def welcome = Action { implicit request =>
+    Ok(html.welcome(loginForm))
+  }
 }
